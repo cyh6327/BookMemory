65da5ee43a78e8d04c39a158428d79f5d23e3b48
cyh6327 <cyh6327@gmail.com>
2023-10-17 18:18:52 +0900

    책 생성 로직 fetch api로 변경

--- bookMemory/src/main/java/com/yh/bookMemory/controller/MainController.java
+++ bookMemory/src/main/java/com/yh/bookMemory/controller/MainController.java
@@ -5,11 +5,15 @@ import com.yh.bookMemory.dto.PageRequestDTO;
 import com.yh.bookMemory.dto.PageResultDTO;
 import com.yh.bookMemory.entity.BookInfo;
 import com.yh.bookMemory.service.BookService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
 import java.util.List;
@@ -21,7 +25,7 @@ public class MainController {
     BookService bookService;
 
     @GetMapping("/dashboard")
-    public String dashboard(Model model) throws IOException {
+    public String dashboard(Model model) {
         log.info("dashboard.......");
 
         PageRequestDTO pageRequestDTO = PageRequestDTO.builder().page(1).size(10).build();
@@ -32,6 +36,12 @@ public class MainController {
         model.addAttribute("list", bookList);
 
         return "/layout/index";
+    }
+
+    @GetMapping("/book/create")
+    public String createBookPage() {
+        log.info("move createBook Page...............");
+        return "createBook";
     }
 
 }
