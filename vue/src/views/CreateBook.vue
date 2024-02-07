448af717f8b06cea311ad12ad99e8255b3ef4d25
cyh6327 <cyh6327@gmail.com>
2024-02-07 18:58:40 +0900

    vue yes24로 받아온 검색결과 보이도록 레이아웃 구성

--- vue/src/views/CreateBook.vue
+++ vue/src/views/CreateBook.vue
@@ -1,9 +1,62 @@
 <template>
-  <v-main>
-    <h1>책 정보 등록 {{ testData }}</h1>
-    <v-container>
-      <v-form id="bookInfoForm" @submit.prevent="onSubmit">
-        <v-row>
+  <v-container class="w-50">
+    <h1 class="mb-5">책 검색 {{ testData }}</h1>
+    <v-row justify="center">
+      <v-col>
+        <v-text-field
+            v-model="keyword"
+            :rules="nameRules"
+            :counter="20"
+            label="검색어를 입력해주세요."
+            clear-icon="mdi mdi-close"
+            clearable
+            required
+            append-inner-icon="mdi-magnify"
+            @click:append-inner="searchBookInfo"
+          ></v-text-field>
+        </v-col>
+    </v-row>
+    <v-row v-if="this.searchBook.length != 0">
+        <v-col cols="4" v-for="book in searchBook" :key="book">
+        <v-card
+            color="#952175"
+            theme="dark"
+        >
+            <div class="card-container d-flex">
+            <div style="width:60%">
+                <v-card-title style="padding: 20px 20px 5px 20px;font-size: 18px;">
+                    {{ book.title }}
+                </v-card-title>
+
+                <v-card-subtitle>{{ book.author }}</v-card-subtitle>
+                
+                <v-card-actions class="d-flex justify-center">
+                    <span v-for="i in book.rating" :key="i" class="material-symbols-outlined">kid_star</span>
+                </v-card-actions>
+    
+                <!-- <span class="tag"><a href="">#{{ book.genre }}</a></span> -->
+
+                <v-card-actions>
+                <v-btn
+                    class="ms-2"
+                ></v-btn>
+                </v-card-actions>
+            </div>
+
+            <v-avatar
+                class="ma-3"
+                size="125"
+                rounded="0"
+            >
+            <v-img :src="book.img"></v-img>
+            </v-avatar>
+            </div>
+        </v-card>
+        </v-col>
+    </v-row>
+  </v-container>
+    <!-- <v-sheet>
+      <v-row>
           <v-col>
             <v-text-field
                 v-model="keyword"
@@ -23,13 +76,15 @@
               />
             </v-col>
         </v-row>
+      </v-sheet> -->
+      <!-- <v-form id="bookInfoForm" @submit.prevent="onSubmit"> -->
         <!-- <v-checkbox 
             v-model="book.favoriteFlag"
             label="즐겨찾기"
             color="orange"
         ></v-checkbox> -->
 
-        <v-text-field
+        <!-- <v-text-field
             v-model="book.title"
             :rules="nameRules"
             :counter="20"
@@ -93,10 +148,9 @@
             :counter="100"
         ></v-textarea>
   
-        <v-btn type="submit" block class="mt-2 float-end" color="#2c3e50">등록</v-btn>
-      </v-form>
-      </v-container>
-  </v-main>
+        <v-btn type="submit" block class="mt-2 float-end" color="#2c3e50">등록</v-btn> 
+      </v-form> -->
+  <!-- </v-main> -->
 </template>
   
 <script>
@@ -108,6 +162,7 @@
         testData : "",
         book: {}, 
         keyword : "",
+        searchBook : {},
       };
     },
     // mounted() {
@@ -135,6 +190,7 @@
           this.axios.post("/book/search/yes24/"+this.keyword)
           .then((res) => {
               console.log(res);
+              this.searchBook = res.data;
           })
         }
     },
@@ -142,9 +198,6 @@
 </script>
 
 <style>
-.v-main {
-  max-width: 700px;
-}
 .v-container.d-flex {
   padding: 0;
   margin-bottom: 10px;
@@ -154,5 +207,54 @@
 }
 .dp__main {
   margin-bottom: 20px;
+}
+.v-card {
+    max-width: 1800px !important;
+    min-height: 200px;
+    background-color: transparent;
+}
+.card-container {
+    height: 200px;
+}
+.v-avatar {
+    width: 40% !important; 
+    height: 100% !important; 
+    padding: 15px; 
+    margin: 0 !important;
+}
+.nav-link {
+  text-decoration: none;
+  color: white;
+}
+.nav-link:hover {
+  text-decoration: none;
+  color: #D0ADF0 !important;
+}
+.card-container{
+    background-color: rgba(33, 33, 33);
+}
+.v-icon {
+    /* color: #D0ADF0 !important; */
+    color: #CFADF0
+}
+.tag a{
+    color: #fff;
+    text-decoration: none;
+}
+.tag a:hover {
+    color: #D0ADF0 !important;
+    text-decoration: none;
+}
+.material-symbols-outlined {
+  font-variation-settings:
+  'FILL' 1,
+  'wght' 200,
+  'GRAD' 0,
+  'opsz' 24;
+  color: #D0ADF0;
+  font-size: 15px;
+}
+.v-btn {
+    color: #D0ADF0 !important;
 }
 </style>
